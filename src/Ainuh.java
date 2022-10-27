0f1e9bfc747a2bddb1a69faac18845a334a4cd4c
unknown <ainaannysa@gmail.com>
2022-10-27 13:09:52 +0800

    first commit

---
+++ src/Ainuh.java
@@ -0,0 +1,19 @@
+import.java.util.Scanner;
+public class Ainuh
+{
+    public static void main(String[] args)
+        {
+           Scanner console = new Scanner(System.in);
+           int num
+
+           System.out.print("enter positive integer");
+           num = console.nextInt();
+
+           System.out.print("multiplication table of "+num);
+
+           for (int i=1; i<=10;  i++)
+           {
+               System.out.println(num +" x " + i + " = " = (num*i)) ;
+            }
+        }
+    }
